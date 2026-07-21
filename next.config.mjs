c0d6b58644f5441887d0a3a3a55d5a64f55e65d4
munazir Raza <139262123+munazir17@users.noreply.github.com>
2026-07-22 00:42:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@x402/evm/upto/client": false,
+      "@x402/evm/exact/client": false,
+      "@x402/core/client": false,
+      "@x402/svm/exact/client": false,
+      "@x402/evm": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
