3e920891ff7a98fdd12bfdefb23f066b9c881ecb
munazir Raza <139262123+munazir17@users.noreply.github.com>
2026-08-15 15:22:28 +0530

    Create index.ts

---
+++ lib/games/mpgr-run/index.ts
@@ -0,0 +1,8 @@
+// lib/games/mpgr-run/index.ts
+//
+// MPGR Run module barrel export.
+
+export * from "./run-config";
+export * from "./run-score";
+export * from "./run-validation";
+export * from "./run-rewards";
