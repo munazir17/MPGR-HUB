163e9ca5d5e576538e9f9b52dc252a942dcf775b
munazir Raza <139262123+munazir17@users.noreply.github.com>
2026-08-21 02:18:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "@/components/providers";
 import { MiniAppAutoConnect } from "@/components/MiniAppAutoConnect";
 import { RecentPageTracker } from "@/components/RecentPageTracker";
+import { ReferralCapture } from "@/components/ReferralCapture";
 import { BottomNav } from "@/components/BottomNav";
 
 export const metadata: Metadata = {
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Providers>
           <MiniAppAutoConnect />
           <RecentPageTracker />
+          <ReferralCapture />
           {children}
           <BottomNav />
         </Providers>
