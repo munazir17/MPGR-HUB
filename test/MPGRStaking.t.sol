bf53a63ced6c01229f897fd488d8156d7366f728
munazir17 <munazir17@users.noreply.github.com>
2026-09-11 03:34:11 +0530

    Fix staking test interface import

--- test/MPGRStaking.t.sol
+++ test/MPGRStaking.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {Test} from "forge-std/Test.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {MPGRStaking} from "../contracts/MPGRStaking.sol";
+import {IMPGRStaking} from "../contracts/interfaces/IMPGRStaking.sol";
 
 contract MockMPGR is ERC20 {
     constructor() ERC20("MPGR", "MPGR") {}
